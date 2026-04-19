a243058a10deb07aab15332a95efcaae282c9ea5
Ashok Babu Bavireddy <130152937+notashock@users.noreply.github.com>
2026-04-19 16:05:52 +0530

    created a repository for respective model

--- server/src/main/java/com/pothole_report/server/repositories/PotholeReportRepository.java
+++ server/src/main/java/com/pothole_report/server/repositories/PotholeReportRepository.java
@@ -3,5 +3,5 @@ package com.pothole_report.server.repositories;
 import com.pothole_report.server.models.PotholeReport;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PotholeReportRepository extends JpaRepository<PotholeReport, Integer> {
+public interface PotholeReportRepository extends JpaRepository<PotholeReport, Long> {
 }
